78864016ea4f5a75cf652aa8e5f11a2fe72d51f5
csaflip <colin.adams@ufl.edu>
2020-09-19 23:39:22 -0400

    Added Dealer class to abstract code, added win logic.

--- test.py
+++ test.py
@@ -26,45 +26,57 @@ class Card:
     def print_card(self):
         return self.card_dict[self.card_num]
 
-class Game:
+class Player:
     def __init__(self):
-        self.dealer_one = Card()
-        self.dealer_two = Card()
-        self.dealer_hand = []
-        self.dealer_hand.append(self.dealer_one)
-        self.dealer_hand.append(self.dealer_two)
+        self.hand = [Card(), Card()]
+    
+    def hit(self):
+        self.hand.append(Card())
 
-        self.player_one = Card()
-        self.player_two = Card()
-        self.player_hand = []
-        self.player_hand.append(self.player_one)
-        self.player_hand.append(self.player_two)
-
-    def get_hand_value(self, hand):
+    def get_hand_value(self):
         total = 0
         
-        for card in hand:
+        for card in self.hand:
             total += card.get_value()
         return total
+
+class Game:
+    def __init__(self):
+        self.Dealer = Player()
+        self.Human = Player()
+
+
 
     def run_game(self):
         gamestate = True
 
         while gamestate:
-            print('You\'ve been dealt a ' + self.player_one.print_card() + '!')
-            print('You\'ve been dealt a ' + self.player_two.print_card() + '!')
+            print('You\'ve been dealt a ' + self.Human.hand[0].print_card() + '!')
+            print('You\'ve been dealt a ' + self.Human.hand[1].print_card() + '!')
             print('Would you like to: 1. Hit 2. Stay')
             response = input()
             if response == '1':
-                self.player_hand.append(Card())
-                print('You got a ' + self.player_hand[-1].print_card())
-                if self.get_hand_value(self.player_hand) > 21:
+                self.Human.hit()
+                print('\nYou got a ' + self.Human.hand[-1].print_card()) # check last card in hand
+                if self.Human.get_hand_value() > 21:
                     gamestate = False
                     print('you fucking suck, you lost all your money')
+                else: # Assuming you didn't lose ;)
+                    if self.Dealer.get_hand_value() <=17: # Dealer must hit if under 17 
+                        self.Dealer.hit()
+
             elif response == '2':
-                if self.get_hand_value(self.player_hand) > self.get_hand_value(self.dealer_hand) and self.get_hand_value(self.player_hand) <= 21:
+
+                if self.Dealer.get_hand_value() <=17: # Dealer must hit if under 17 
+                    self.Dealer.hit()
+
+                if self.Human.get_hand_value() > self.Dealer.get_hand_value() and self.Human.get_hand_value() <= 21 or self.Dealer.get_hand_value() > 21:
+                    print('Your hand: ' + str(self.Human.get_hand_value()))
+                    print('Dealer hand: ' + str(self.Dealer.get_hand_value()))
                     print('You win you fat fucker')
                 else:
+                    print('Your hand: ' + str(self.Human.get_hand_value()))
+                    print('Dealer hand: ' + str(self.Dealer.get_hand_value()))
                     print('You Lose')
                 gamestate = False
 
